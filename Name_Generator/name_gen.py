b9c54a6f036a03a90b1715928b8a0a14ee4e128c
Darren Dhanpat <darren.dhanpat@yahoo.com>
2019-01-22 22:51:10 -0500

    updated python training files

---
+++ Name_Generator/name_gen.py
@@ -0,0 +1,7 @@
+import sys, random
+
+print("Welcome to the Psych 'Sidekick Picker.'\n")
+print("A name just like John would pick for Terrence:\n\n")
+
+first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill Beenie-Weenie'",
+         "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite' ",)
--- python_dictionaries/dictonary_training.py
+++ python_dictionaries/dictonary_training.py
@@ -1,3 +1,35 @@
+#Dictionaries have key:value pairs and each dictionary are seperated by a comma in a single dictionary
 alien_0 = {'color': 'green', 'points': 5}
 print(alien_0['color'])
 print(alien_0['points'])
+
+new_points = alien_0['points']
+print("You just earned " + str(new_points) + " points!")
+
+#update our aliens with x and y coordinates
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+print("empty space\n")
+#adding items to an empty dict
+alien_0 = {}
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+print(alien_0)
+
+print("#")
+#changing a value to another value in a dict
+alien_0 = {'color': 'green'}
+print("The color is " + alien_0['color'] + ".")
+
+alien_0['color'] = 'yellow'
+print("The color is now " + alien_0['color'] + ".")
+
+
+print('#\n')
+print()
+
